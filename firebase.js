3e7e9fed52c70cba45427e6918ca8ae1604660e1
ulfaalhaqq <ulfaalhaq29@gmail.com>
2026-05-21 17:53:40 +0700

    fix conflict firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
@@ -13,32 +12,9 @@ const firebaseConfig = {
   appId: "1:685630209243:web:df25d7f2f91b390a2820fe"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
 
-=======
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBnrY7oq-CkzBU66ggiu2H3h-46L7VPePc",
-  authDomain: "my-notes-app-7945c.firebaseapp.com",
-  projectId: "my-notes-app-7945c",
-  storageBucket: "my-notes-app-7945c.firebasestorage.app",
-  messagingSenderId: "685630209243",
-  appId: "1:685630209243:web:df25d7f2f91b390a2820fe"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const provider = new GoogleAuthProvider();
-
->>>>>>> e0146532a6f3aca49367de7482306c1063972add
 export { auth, db, provider };
