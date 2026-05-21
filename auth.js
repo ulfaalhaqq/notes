62c531e7c9d8eba05e5e3248083e7e1f08e1a926
ulfaalhaqq <ulfaalhaq29@gmail.com>
2026-05-21 17:28:47 +0700

    first commit

---
+++ auth.js
@@ -0,0 +1,24 @@
+import { auth, provider } from "./firebase.js";
+import { signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    window.location.href = "dashboard.html";
+  }
+});
+
+const btnGoogle = document.getElementById("btnGoogle");
+const errorMsg = document.getElementById("errorMsg");
+
+btnGoogle.addEventListener("click", async () => {
+  try {
+    btnGoogle.disabled = true;
+    btnGoogle.innerText = "Memuat...";
+    await signInWithPopup(auth, provider);
+  } catch (error) {
+    errorMsg.classList.remove("hidden");
+    errorMsg.innerText = "Login gagal: " + error.message;
+    btnGoogle.disabled = false;
+    btnGoogle.innerText = "Masuk dengan Google";
+  }
+});
